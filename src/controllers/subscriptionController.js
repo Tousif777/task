935064828eb6b378165d746b695381cf346b1e1f
Tousif <tousifhossain.777@gmail.com>
2023-11-18 14:51:13 +0600

    added auto renewal and off auto renewal

--- src/controllers/subscriptionController.js
+++ src/controllers/subscriptionController.js
@@ -150,9 +150,149 @@ const getPaymentHistory = async (req, res) => {
     }
 };
 
+const turnOffAutoRenewal = async (req, res) => {
+    try {
+        // Get the access token from the request header
+        const accessToken = req.headers.token;
+
+        // Decode the access token to get user information
+        const decodedToken = decodeAccessToken(accessToken);
+
+        if (!decodedToken) {
+            return res.status(401).json({ status: false, message: 'Unauthorized' });
+        }
+
+        // Retrieve the customer from Stripe using the user's email
+        const userMail = decodedToken.email;
+
+        // Retrieve the customer ID using the email address
+        const customer = await stripe.customers.list({
+            email: userMail,
+            limit: 1,
+        });
+
+        if (customer.data.length === 0) {
+            return res.status(404).json({ status: false, message: 'Customer not found' });
+        }
+
+        const customerId = customer.data[0].id;
+
+        // Retrieve the customer's subscriptions
+        const subscriptions = await stripe.subscriptions.list({
+            customer: customerId,
+        });
+
+        // If the customer has subscriptions, turn off auto-renewal for the specified one
+        if (subscriptions.data.length > 0) {
+            // Assuming the subscription ID is provided in the request body
+            const { subscriptionIdToTurnOffAutoRenewal } = req.body;
+
+            if (!subscriptionIdToTurnOffAutoRenewal) {
+                return res.status(400).json({ status: false, message: 'Subscription ID to turn off auto-renewal is missing in the request body' });
+            }
+
+            // Find the subscription in the customer's subscriptions
+            const subscriptionToTurnOffAutoRenewal = subscriptions.data.find(subscription => subscription.id === subscriptionIdToTurnOffAutoRenewal);
+
+            if (subscriptionToTurnOffAutoRenewal) {
+                // Update the subscription to turn off auto-renewal
+                await stripe.subscriptions.update(subscriptionIdToTurnOffAutoRenewal, {
+                    items: [{
+                        id: subscriptionToTurnOffAutoRenewal.items.data[0].id,
+                        price: subscriptionToTurnOffAutoRenewal.items.data[0].price.id,
+                        quantity: 1,
+                        billing_thresholds: {
+                            usage_gte: 1,
+                        },
+                    }],
+                });
+                res.json({ status: true, message: 'Auto-renewal turned off successfully for the subscription' });
+            } else {
+                res.status(404).json({ status: false, message: 'Subscription not found for the customer' });
+            }
+        } else {
+            res.json({ status: false, message: 'No active subscriptions to turn off auto-renewal' });
+        }
+    } catch (error) {
+        console.error('Error turning off auto-renewal for subscription:', error.message);
+        res.status(500).json({ error: 'Error turning off auto-renewal for subscription' });
+    }
+};
+
+const turnOnAutoRenewal = async (req, res) => {
+    try {
+        // Get the access token from the request header
+        const accessToken = req.headers.token;
+
+        // Decode the access token to get user information
+        const decodedToken = decodeAccessToken(accessToken);
+
+        if (!decodedToken) {
+            return res.status(401).json({ status: false, message: 'Unauthorized' });
+        }
+
+        // Retrieve the customer from Stripe using the user's email
+        const userMail = decodedToken.email;
+
+        // Retrieve the customer ID using the email address
+        const customer = await stripe.customers.list({
+            email: userMail,
+            limit: 1,
+        });
+
+        if (customer.data.length === 0) {
+            return res.status(404).json({ status: false, message: 'Customer not found' });
+        }
+
+        const customerId = customer.data[0].id;
+
+        // Retrieve the customer's subscriptions
+        const subscriptions = await stripe.subscriptions.list({
+            customer: customerId,
+        });
+
+        // If the customer has subscriptions, turn on auto-renewal for the specified one
+        if (subscriptions.data.length > 0) {
+            // Assuming the subscription ID is provided in the request body
+            const { subscriptionIdToTurnOnAutoRenewal } = req.body;
+
+            if (!subscriptionIdToTurnOnAutoRenewal) {
+                return res.status(400).json({ status: false, message: 'Subscription ID to turn on auto-renewal is missing in the request body' });
+            }
+
+            // Find the subscription in the customer's subscriptions
+            const subscriptionToTurnOnAutoRenewal = subscriptions.data.find(subscription => subscription.id === subscriptionIdToTurnOnAutoRenewal);
+
+            if (subscriptionToTurnOnAutoRenewal) {
+                // Update the subscription to turn on auto-renewal
+                await stripe.subscriptions.update(subscriptionIdToTurnOnAutoRenewal, {
+                    items: [{
+                        id: subscriptionToTurnOnAutoRenewal.items.data[0].id,
+                        price: subscriptionToTurnOnAutoRenewal.items.data[0].price.id,
+                        quantity: 1,
+                        billing_thresholds: {
+                            usage_gte: 1,
+                        },
+                    }],
+                });
+                res.json({ status: true, message: 'Auto-renewal turned on successfully for the subscription' });
+            } else {
+                res.status(404).json({ status: false, message: 'Subscription not found for the customer' });
+            }
+        } else {
+            res.json({ status: false, message: 'No active subscriptions to turn on auto-renewal' });
+        }
+    } catch (error) {
+        console.error('Error turning on auto-renewal for subscription:', error.message);
+        res.status(500).json({ error: 'Error turning on auto-renewal for subscription' });
+    }
+};
+
 
 module.exports = {
     getSubscriptions,
     cancelSubscription,
-    getPaymentHistory
+    getPaymentHistory,
+    turnOffAutoRenewal,
+    turnOnAutoRenewal
 };
--- src/routes/subscriptionRoute.js
+++ src/routes/subscriptionRoute.js
@@ -5,5 +5,7 @@ const subscriptionController = require('../controllers/subscriptionController');
 router.get('/get-subscriptions', subscriptionController.getSubscriptions);
 router.post('/cancel-subscription', subscriptionController.cancelSubscription);
 router.get('/payment-history', subscriptionController.getPaymentHistory)
+router.post('/auto-renew-off', subscriptionController.turnOffAutoRenewal)
+router.post('/auto-renew-on', subscriptionController.turnOnAutoRenewal)
 
 module.exports = router;
